a6cb0ee10df04ceed9746f4cde3453a3ee8eba53
Jesper Evertsson <jesper@piktiv.se>
2023-12-03 22:20:11 +0100

    Add solutions for day 3 part 2

--- src/main/kotlin/day3.kt
+++ src/main/kotlin/day3.kt
@@ -2,11 +2,76 @@ fun day3 (lines: List<String>) {
     val partNumbers = findPartNumbers(lines)
 
     markInvalidPartNumbers(lines, partNumbers)
+    findAdjacentGears(lines, partNumbers)
     
     val partNumberSum = partNumbers.filter { it.isValidPartNumber }.sumOf { it.number }
+    
+    val gearRatioSum = calculateGearRatioSum(partNumbers)
 
     println("Day 3 part 1: $partNumberSum")
-    println("Day 3 part 2:")
+    println("Day 3 part 2: $gearRatioSum")
+}
+
+fun findAdjacentGears(lines: List<String>, partNumbers: List<PartNumber>) {
+    partNumbers.forEach {
+        val gearCoordinate = anyAdjacentGear(lines, it)
+        if(gearCoordinate != null) {
+            it.gearCoordinate = gearCoordinate
+        }
+    }
+}
+
+fun anyAdjacentGear(lines: List<String>, partNumber: PartNumber): Coordinate? {
+    for (i in partNumber.startX..<partNumber.startX + partNumber.length) {
+        val gearCoordinate = findAdjacentGear(lines, i , partNumber.y)
+        if (gearCoordinate != null) {
+            return gearCoordinate
+        }
+    }
+    
+    return null
+}
+
+fun findAdjacentGear(lines: List<String>, x: Int, y: Int): Coordinate? {
+    for (xi in x-1..x+1) {
+        for (yi in y-1..y+1) {
+            try {
+                if (lines[yi][xi] == '*') {
+                    return Coordinate(xi, yi)
+                }
+            } catch (e: IndexOutOfBoundsException) {
+                continue
+            }
+        }
+    }
+    
+    return null
+}
+
+fun calculateGearRatioSum(partNumbers: List<PartNumber>): Long {
+    var gearRatioSum = 0L
+    
+    for (i in partNumbers.indices) {
+        if (partNumbers[i].gearCoordinate == null) {
+            continue
+        }
+        val numbersWithGear = mutableListOf(partNumbers[i].number)
+        for (j in partNumbers.indices) {
+            if (i == j) {
+                continue
+            }
+            
+            if (partNumbers[i].gearCoordinate == partNumbers[j].gearCoordinate) {
+                numbersWithGear.add(partNumbers[j].number)
+            }
+        }
+        
+        if (numbersWithGear.size == 2) {
+            gearRatioSum += (numbersWithGear[0] * numbersWithGear[1])
+        }
+    }
+    
+    return gearRatioSum / 2
 }
 
 fun markInvalidPartNumbers(lines: List<String>, partNumbers: List<PartNumber>) {
@@ -53,13 +118,13 @@ fun findPartNumbers(lines: List<String>): List<PartNumber> {
                 numberBuilder += cell
             } else {
                 if (numberBuilder.isNotEmpty()) {
-                    partNumbers.add(PartNumber(Integer.parseInt(numberBuilder), xIndex - numberBuilder.length, numberBuilder.length, yIndex, false))
+                    partNumbers.add(PartNumber(Integer.parseInt(numberBuilder), xIndex - numberBuilder.length, numberBuilder.length, yIndex, false, null))
                 }
                 numberBuilder = ""
             }
             
             if (xIndex == row.length - 1 && numberBuilder.isNotEmpty()) {
-                partNumbers.add(PartNumber(Integer.parseInt(numberBuilder), xIndex - numberBuilder.length, numberBuilder.length, yIndex, false))
+                partNumbers.add(PartNumber(Integer.parseInt(numberBuilder), xIndex - numberBuilder.length, numberBuilder.length, yIndex, false, null))
             }
         }
     }
@@ -67,6 +132,6 @@ fun findPartNumbers(lines: List<String>): List<PartNumber> {
     return partNumbers
 }
 
-data class PartNumber (val number: Int, val startX: Int, val length: Int, val y: Int, var isValidPartNumber: Boolean)
+data class PartNumber (val number: Int, val startX: Int, val length: Int, val y: Int, var isValidPartNumber: Boolean, var gearCoordinate: Coordinate?)
 
-//data class Coordinate (val x: Int, val y: Int)
+data class Coordinate (val x: Int, val y: Int)
