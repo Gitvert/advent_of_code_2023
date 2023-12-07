8497b6a265840762a092ece4324edd0dfbe968f9
Jesper Evertsson <jesper@piktiv.se>
2023-12-07 18:12:57 +0100

    Add solutions for day 7 part 2

--- src/main/kotlin/day7.kt
+++ src/main/kotlin/day7.kt
@@ -1,10 +1,38 @@
 fun day7 (lines: List<String>) {
+    day7part1(lines)
+    day7part2(lines)
+}
 
+fun day7part2(lines: List<String>) {
+    val cards = parseCamelCards(lines)
+
+    cards.forEach { it.handStrength = findHandStrength(replaceJokers(it)) }
+
+    cards.forEach {
+        it.cards = it.cards
+            .replace("A", "Z")
+            .replace("K", "Y")
+            .replace("Q", "X")
+            .replace("J", "1")
+            .replace("T", "U")
+    }
+
+    var sum = 0L
+
+    cards.sortedWith(compareBy({ it.handStrength }, { it.cards })).forEachIndexed { index, card ->
+        sum += (index + 1) * card.bid
+    }
+
+    println("Day 7 part 2: $sum")
+    println() 
+}
+
+fun day7part1(lines: List<String>) {
     val cards = parseCamelCards(lines)
     
     cards.forEach { it.handStrength = findHandStrength(it) }
-    
-    cards.forEach { 
+
+    cards.forEach {
         it.cards = it.cards
             .replace("A", "Z")
             .replace("K", "Y")
@@ -12,35 +40,31 @@ fun day7 (lines: List<String>) {
             .replace("J", "V")
             .replace("T", "U")
     }
-    
+
     var sum = 0L
-    
+
     cards.sortedWith(compareBy({ it.handStrength }, { it.cards })).forEachIndexed { index, card ->
         sum += (index + 1) * card.bid
-    } 
-    
+    }
+
     println("Day 7 part 1: $sum")
-    println("Day 7 part 2: ")
-    println()
 }
 
 fun findHandStrength(hand: CamelCardsHand): Int {
-    val cardOccurrences = hand.cards.groupingBy { it }.eachCount()
+    val sortedCardOccurrences = hand.cards.groupingBy { it }.eachCount().entries.map { it.value.toString()[0] }.sortedDescending()
     
-    val sortedCardOccurrences = cardOccurrences.entries.map { it.value.toString() + it.key.toString() }.sortedDescending()
-    
-    if (sortedCardOccurrences[0][0] == '5') {
+    if (sortedCardOccurrences[0] == '5') {
         return 7
-    } else if (sortedCardOccurrences[0][0] == '4') {
+    } else if (sortedCardOccurrences[0] == '4') {
         return 6
-    } else if (sortedCardOccurrences[0][0] == '3') {
-        return if (sortedCardOccurrences[1][0] == '2') {
+    } else if (sortedCardOccurrences[0] == '3') {
+        return if (sortedCardOccurrences[1] == '2') {
             5
         } else {
             4
         }
-    } else if (sortedCardOccurrences[0][0] == '2') {
-        return if (sortedCardOccurrences[1][0] == '2') {
+    } else if (sortedCardOccurrences[0] == '2') {
+        return if (sortedCardOccurrences[1] == '2') {
             3
         } else {
             2
@@ -48,6 +72,24 @@ fun findHandStrength(hand: CamelCardsHand): Int {
     } else {
         return 1
     }
+}
+
+fun replaceJokers(hand: CamelCardsHand): CamelCardsHand {
+    val sortedCardOccurrences = hand.cards.groupingBy { it }.eachCount().entries.map { it.value.toString() + it.key.toString() }.sortedDescending()
+    
+    if (hand.cards.contains("J")) {
+        for (i in sortedCardOccurrences.indices) {
+            if (!sortedCardOccurrences[i].contains("J")) {
+                return CamelCardsHand(
+                    hand.cards.replace("J", sortedCardOccurrences[i][1].toString()),
+                    hand.bid,
+                    0
+                )
+            }
+        }
+    }
+    
+    return hand
 }
 
 fun parseCamelCards(lines: List<String>): List<CamelCardsHand> {
