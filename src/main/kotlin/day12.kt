b8db98018f1f0e19023e249558b5a4647cdb349f
Jesper Evertsson <jesper@piktiv.se>
2023-12-12 19:10:32 +0100

    Add solutions for day 12 part 1

--- src/main/kotlin/day12.kt
+++ src/main/kotlin/day12.kt
@@ -1,3 +1,40 @@
 fun day12 (lines: List<String>) {
+    var totalArrangements = 0L
     
+    lines.forEach { line ->
+        totalArrangements += findArrangements(line.split(" ")[0], line.split(" ")[1].split(",").map { Integer.parseInt(it) })
+    }
+
+    println("Day 12 part 1: $totalArrangements")
+    println("Day 12 part 2: ")
+    println()
+}
+
+fun findArrangements(springs: String, groups: List<Int>): Long {
+    if (springs.contains("?")) {
+        return findArrangements(springs.replaceFirst("?", "."), groups) + findArrangements(springs.replaceFirst("?", "#"), groups)
+    }
+    
+    var springGroupCount = 0
+    val foundGroups = mutableListOf<Int>()
+    
+    springs.forEach { 
+        if (it == '#') {
+            springGroupCount++
+        } else if (it == '.' && springGroupCount > 0) {
+            foundGroups.add(springGroupCount)
+            springGroupCount = 0
+        }
+    }
+
+    if (springGroupCount > 0) {
+        foundGroups.add(springGroupCount)
+        springGroupCount = 0
+    }
+
+    return if (foundGroups == groups) {
+        1
+    } else {
+        0
+    }
 }
