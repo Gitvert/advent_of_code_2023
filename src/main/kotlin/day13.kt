05f50fa1944175728f2365107f689a0222350c38
Jesper Evertsson <jesper@piktiv.se>
2023-12-13 23:23:40 +0100

    Add solutions for day 13 part 2

--- src/main/kotlin/day13.kt
+++ src/main/kotlin/day13.kt
@@ -1,38 +1,99 @@
+val originalReflection = mutableMapOf<Int, Mirror>()
+
 fun day13 (lines: List<String>) {
     
     val patterns = parsePatterns(lines)
     var sum = 0L
     
-    patterns.forEach { 
-        var newSum = findHorizontalReflectionSum(it, 100) 
+    patterns.forEachIndexed { index, it -> 
+        var newSum = findHorizontalReflectionSum(it, 100, true, index) 
         if (newSum == 0) {
-            newSum = findVerticalReflectionSum(it)
+            newSum = findVerticalReflectionSum(it, true, index)
         }
-        println(newSum)
         sum += newSum
     }
 
     println("Day 13 part 1: $sum")
-    println("Day 13 part 2: ")
+    var smudgeSum = 0L
+    var hits = 0
+    
+    patterns.forEachIndexed { index, pattern ->
+        var horizontalSum: Int
+        var verticalSum: Int
+        for (row in pattern.pattern.indices) {
+            for (col in pattern.pattern[row].indices) {
+                horizontalSum = findHorizontalReflectionSum(replaceSmudge(pattern, row, col), 100, false, index)
+                verticalSum = findVerticalReflectionSum(replaceSmudge(pattern, row, col), false, index)
+                
+                if (horizontalSum != 0) {
+                    smudgeSum += horizontalSum
+                    hits++
+                    return@forEachIndexed
+                }
+                
+                if (verticalSum != 0) {
+                    smudgeSum += verticalSum
+                    hits++
+                    return@forEachIndexed
+                }
+            }
+            
+        }
+    }
+    
+    println("Day 13 part 2: $smudgeSum")
     println()
 }
 
-fun findHorizontalReflectionSum(pattern: List<String>, multiplier: Int): Int {
-    for (i in pattern.indices) {
-        if (i == pattern.size - 1) {
+fun replaceSmudge(pattern: Pattern, row: Int, col: Int): Pattern {
+    val replaced = Pattern(mutableListOf())
+    
+    for (y in pattern.pattern.indices) {
+        var line = ""
+        for (x in pattern.pattern[y].indices) {
+            line += if (y == row && x == col) {
+                if (pattern.pattern[y][x] == '.') { '#' } else { '.' }
+            } else {
+                pattern.pattern[y][x]
+            }
+        }
+        replaced.pattern.add(line)
+    }
+    
+    return replaced
+}
+
+fun findHorizontalReflectionSum(pattern: Pattern, multiplier: Int, part1: Boolean, index: Int): Int {
+    for (i in pattern.pattern.indices) {
+        if (i == pattern.pattern.size - 1) {
             continue
         }
         
-        if (pattern[i] == pattern[i + 1]) {
+        if (!part1 && multiplier == 1 && originalReflection[index]!!.originalVerticalLine != null && originalReflection[index]!!.originalVerticalLine == i) {
+            continue
+        }
+
+        if (!part1 && multiplier == 100 && originalReflection[index]!!.originalHorizontalLine != null && originalReflection[index]!!.originalHorizontalLine == i) {
+            continue
+        }
+        
+        if (pattern.pattern[i] == pattern.pattern[i + 1]) {
             var matching = true
             var indexOffset = 1
 
             try {
                 while (matching) {
-                    matching = pattern[i - indexOffset + 1] == pattern[i + indexOffset]
+                    matching = pattern.pattern[i - indexOffset + 1] == pattern.pattern[i + indexOffset]
                     indexOffset++
                 }
             } catch (e: IndexOutOfBoundsException) {
+                if (part1) {
+                    if (multiplier == 1) {
+                        originalReflection[index] = Mirror(null, i)
+                    } else {
+                        originalReflection[index] = Mirror(i, null)
+                    }
+                }
                 return (i + 1) * multiplier
             }
         }
@@ -41,16 +102,16 @@ fun findHorizontalReflectionSum(pattern: List<String>, multiplier: Int): Int {
     return 0
 }
 
-fun rotatePattern(pattern: List<String>): List<String> {
-    val rotated = mutableListOf<String>()
+fun rotatePattern(pattern: Pattern): Pattern {
+    val rotated = Pattern(mutableListOf())
 
-    pattern[0].forEach { _ ->
-        rotated.add("")
+    pattern.pattern[0].forEach { _ ->
+        rotated.pattern.add("")
     }
 
-    pattern.reversed().forEach { row ->
+    pattern.pattern.reversed().forEach { row ->
         row.forEachIndexed { index, c ->
-            rotated[index] = rotated[index] + c
+            rotated.pattern[index] = rotated.pattern[index] + c
         }
         
     }
@@ -58,21 +119,21 @@ fun rotatePattern(pattern: List<String>): List<String> {
     return rotated
 }
 
-fun findVerticalReflectionSum(pattern: List<String>): Int {
-    return findHorizontalReflectionSum(rotatePattern(pattern), 1)
+fun findVerticalReflectionSum(pattern: Pattern, part1: Boolean, index: Int): Int {
+    return findHorizontalReflectionSum(rotatePattern(pattern), 1, part1, index)
 }
 
-fun parsePatterns(lines: List<String>): List<List<String>> {
-    val patterns = mutableListOf<MutableList<String>>()
+fun parsePatterns(lines: List<String>): List<Pattern> {
+    val patterns = mutableListOf<Pattern>()
     
-    var pattern = mutableListOf<String>()
+    var pattern = Pattern(mutableListOf())
     
     lines.forEach { 
         if (it.isEmpty()) {
             patterns.add(pattern)
-            pattern = mutableListOf()
+            pattern = Pattern(mutableListOf())
         } else {
-            pattern.add(it)
+            pattern.pattern.add(it)
         }
     }
     
@@ -80,3 +141,7 @@ fun parsePatterns(lines: List<String>): List<List<String>> {
     
     return patterns
 }
+
+data class Pattern(val pattern: MutableList<String>)
+
+data class Mirror(val originalHorizontalLine: Int?, val originalVerticalLine: Int?)
