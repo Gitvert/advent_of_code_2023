0b20202231925c1991ad6bc264a0ff5008a26596
Jesper Evertsson <jesper@piktiv.se>
2023-12-22 16:44:50 +0100

    Add notes for day 21 part 2

--- src/main/kotlin/day21.kt
+++ src/main/kotlin/day21.kt
@@ -1,15 +1,42 @@
 fun day21 (lines: List<String>) {
+    part1(lines)
+    part2(lines)
+}
+
+fun part1(lines: List<String>) {
     val start = findStartPosition(lines)
     val currentPositions = mutableSetOf(start)
     val maxX = lines[0].indices.last
     val maxY = lines.indices.last
-    
+
     for (i in 1..64) {
         takeAllPossibleSteps(lines, currentPositions, maxX, maxY)
     }
 
     println("Day 21 part 1: ${currentPositions.size}")
-    println("Day 21 part 2: ")
+}
+
+fun part2(lines: List<String>) {
+    val extendedMap = extendMap(lines)
+    //printGarden(extendedMap, mutableSetOf())
+
+    val start = findStartPosition(extendedMap)
+    println(start)
+    val currentPositions = mutableSetOf(start)
+    val maxX = extendedMap[0].indices.last
+    val maxY = extendedMap.indices.last
+
+    
+    //Repeats at 65 + 131 * n
+    for (i in 1..65) {
+        takeAllPossibleSteps(extendedMap, currentPositions, maxX, maxY)
+        //println("${currentPositions.size}")
+    }
+    
+    //No of clustered rocks contained can be given by formula: (4 * n * n) - (4 * n) + 1
+    printGarden(extendedMap, currentPositions)
+
+    println("Day 21 part 2: ${currentPositions.size}")
     println()
 }
 
@@ -30,7 +57,7 @@ fun takeAllPossibleSteps(lines: List<String>, currentPositions: MutableSet<Pos>,
 }
 
 fun addPositionIfValid(position: Pos, lines: List<String>, newPositions: MutableSet<Pos>, maxX: Int, maxY: Int) {
-    if (position.x in 0..maxX && position.y in 0..maxY && lines[position.y][position.x] != '#') {
+    if (position.x in 0..maxX && position.y in 0..maxY/* && lines[position.y][position.x] != '#'*/) {
         newPositions.add(position)
     }
 }
@@ -41,11 +68,34 @@ fun printGarden(lines: List<String>, currentPositions: MutableSet<Pos>) {
             if (currentPositions.contains(Pos(x, y))) {
                 print('O')
             } else {
-                print('.')
+                print(lines[y][x])
             }
         }
         println()
     }
+}
+
+fun extendMap(lines: List<String>): List<String> {
+    val newMap = mutableListOf<String>()
+    var newLine = ""
+
+    for (i in 1..9) {
+        lines.forEach { line ->
+            for (j in 1..9) {
+                line.forEach{ cell ->
+                    newLine += if (cell == 'S' && (i != 5 || j != 5)) {
+                        '.'
+                    } else {
+                        cell
+                    }
+                }
+            }
+            newMap.add(newLine)
+            newLine = ""
+        }
+    }
+    
+    return newMap
 }
 
 fun findStartPosition(lines: List<String>): Pos {
