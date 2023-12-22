7a30820b374da1a0a9b4a528e513122524b48c65
Jesper Evertsson <jesper@piktiv.se>
2023-12-22 18:59:38 +0100

    Add solutions for day 22 part 1

--- src/main/kotlin/day22.kt
+++ src/main/kotlin/day22.kt
@@ -1,3 +1,131 @@
+import javax.swing.text.html.HTML.Tag.I
+import kotlin.math.max
+import kotlin.math.min
+
 fun day22 (lines: List<String>) {
+
+    val bricks = parseBricks(lines)
+    val settledBricks = settleBricks(bricks)
+    val safeToDisintegrate = findNoOfSafeToDisintegrate(settledBricks)
     
+    println("Day 22 part 1: $safeToDisintegrate")
+    println("Day 22 part 2: ")
+    println()
 }
+
+fun findNoOfSafeToDisintegrate(bricks: List<Brick>): Int {
+    var safeToDisintegrate = 0
+    
+    bricks.forEach { brick ->
+        val settledBricks = settleBricks(bricks.filterNot { brick == it }).toMutableList()
+
+        if (settledBricks == bricks.filterNot { brick == it }) {
+            safeToDisintegrate++
+        }
+    }
+    
+    return safeToDisintegrate
+}
+
+fun settleBricks(bricks: List<Brick>): List<Brick> {
+    var settledBricks = bricks.toMutableList()
+    val changedBricks = mutableListOf<Brick>()
+
+    while (true) {
+        for (i in settledBricks.indices) {
+            val minZ = min(settledBricks[i].start.z, settledBricks[i].end.z)
+            if (minZ == 1) {
+                changedBricks.add(settledBricks[i])
+                continue
+            }
+
+            var isBlocked = false
+            for (j in settledBricks.indices) {
+                if (i == j) {
+                    continue
+                }
+
+                if (restsOn(settledBricks[i], settledBricks[j])) {
+                    isBlocked = true
+                }
+            }
+
+            if (!isBlocked) {
+                changedBricks.add(moveDown(settledBricks[i]))
+            } else {
+                changedBricks.add(settledBricks[i])
+            }
+        }
+
+        if (settledBricks == changedBricks) {
+            break
+        }
+        settledBricks = changedBricks.toMutableList()
+        changedBricks.clear()
+    }
+    
+    return settledBricks
+}
+
+fun moveDown(brick: Brick): Brick {
+    return Brick(
+        Point(brick.start.x, brick.start.y, brick.start.z - 1),
+        Point(brick.end.x, brick.end.y, brick.end.z - 1),
+    )
+}
+
+fun restsOn(current: Brick, other: Brick): Boolean {
+    val currentMinZ = min(current.start.z, current.end.z)
+    val otherMaxZ = max(other.start.z, other.end.z)
+    
+    if (currentMinZ - 1 != otherMaxZ) {
+        return false
+    }
+    
+    val currentPositionsToCheck = generateAllPositionsToCheck(current)
+    val otherPositionsToCheck = generateAllPositionsToCheck(other)
+
+    return currentPositionsToCheck.any { it in otherPositionsToCheck.toSet() }
+}
+
+fun generateAllPositionsToCheck(brick: Brick): List<Pos> {
+    val allPositions = mutableListOf<Pos>()
+    
+    if (brick.start.x == brick.end.x) {
+        val from = min(brick.start.y, brick.end.y)
+        val to = max(brick.start.y, brick.end.y)
+        
+        for (i in from..to) {
+            allPositions.add(Pos(brick.start.x, i))
+        }
+    } else if (brick.start.y == brick.end.y) {
+        val from = min(brick.start.x, brick.end.x)
+        val to = max(brick.start.x, brick.end.x)
+
+        for (i in from..to) {
+            allPositions.add(Pos(i, brick.start.y))
+        }
+    }
+    
+    return allPositions
+}
+
+fun parseBricks(lines: List<String>): MutableList<Brick> {
+    val bricks = mutableListOf<Brick>()
+    
+    lines.forEach { 
+        val startString = it.split("~")[0]
+        val endString = it.split("~")[1]
+        val startNumbers = startString.split(",")
+        val endNumbers = endString.split(",")
+        val start = Point(Integer.parseInt(startNumbers[0]), Integer.parseInt(startNumbers[1]), Integer.parseInt(startNumbers[2]))
+        val end = Point(Integer.parseInt(endNumbers[0]), Integer.parseInt(endNumbers[1]), Integer.parseInt(endNumbers[2]))
+        bricks.add(Brick(start, end))
+    }
+    
+    return bricks
+}
+
+data class Brick(val start: Point, val end: Point)
+
+data class Point(val x: Int, val y: Int, val z: Int)
---
+++ src/test/kotlin/Day22Test.kt
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+val BRICK_A = Brick(Point(1, 0, 1), Point(1, 2, 1))
+val BRICK_B = Brick(Point(0, 0, 2), Point(2, 0, 2))
+val BRICK_F = Brick(Point(0, 1, 6), Point(2, 1, 6))
+val BRICK_G = Brick(Point(1, 1, 7), Point(1, 1, 8))
+
+internal class Day22Test {
+    @Test
+    fun should_return_correct_for_x_direction_brick() {
+        val actual = generateAllPositionsToCheck(Brick(Point(0, 0, 0), Point(2, 0, 0)))
+        
+        assertEquals(listOf(Pos(0, 0), Pos(1, 0), Pos(2, 0)), actual)
+    }
+    
+    @Test
+    fun should_return_correct_for_y_direction_brick() {
+        val actual = generateAllPositionsToCheck(Brick(Point(0, 0, 0), Point(0, 1, 0)))
+
+        assertEquals(listOf(Pos(0, 0), Pos(0, 1)), actual)
+    }
+    
+    @Test
+    fun should_return_correct_for_z_direction_brick() {
+        val actual = generateAllPositionsToCheck(Brick(Point(2, 2, 0), Point(2, 2, 5)))
+
+        assertEquals(listOf(Pos(2, 2)), actual)
+    }
+    
+    @Test
+    fun should_work_when_brick_is_listed_in_reverse_order() {
+        val actual = generateAllPositionsToCheck(Brick(Point(2, 0, 0), Point(0, 0, 0)))
+
+        assertEquals(listOf(Pos(0, 0), Pos(1, 0), Pos(2, 0)), actual)
+    }
+    
+    @Test
+    fun brick_b_should_rest_on_brick_a() {
+        assertTrue(restsOn(BRICK_B, BRICK_A))
+    }
+    
+    @Test
+    fun brick_a_should_not_rest_on_brick_b() {
+        assertFalse(restsOn(BRICK_A, BRICK_B))
+    }
+    
+    @Test
+    fun brick_g_should_rest_of_brick_f() {
+        assertTrue(restsOn(BRICK_G, BRICK_F))
+    }
+}
