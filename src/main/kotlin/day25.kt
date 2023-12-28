5adde532248b42207158a6be1883d4ad5e993456
Jesper Evertsson <jesper@piktiv.se>
2023-12-28 14:14:52 +0100

    Add parsing for day 25 part 1

--- src/main/kotlin/day25.kt
+++ src/main/kotlin/day25.kt
@@ -1,3 +1,29 @@
 fun day25 (lines: List<String>) {
+    val components = parseComponentConnections(lines)
+
+    println("Day 25 part 1: ")
+    println("Day 25 part 2: ")
+    println()
+}
+
+fun parseComponentConnections(lines: List<String>): MutableMap<String, MutableSet<String>> {
+    val components = mutableMapOf<String, MutableSet<String>>()
     
+    lines.forEach { line ->
+        line.replace(":", "").split(" ").forEach { 
+            components[it] = mutableSetOf()
+        }
+    }
+    
+    lines.forEach { line ->
+        val from = line.split(": ")[0]
+        val to = line.split(": ")[1].split(" ")
+        
+        to.forEach { 
+            components[from]?.add(it)
+            components[it]?.add(from)
+        }
+    }
+    
+    return components
 }
